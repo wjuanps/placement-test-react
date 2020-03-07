6e9af3aac17321148a9a5a017f2f69b4ffeda926
Juan Soares <wjuan.ps@gmail.com>
2020-03-07 00:07:18 -0300

    Placement

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'http://localhost:8000/api/';
+const baseURL = "http://localhost:8000/api/";
 
 const api = axios.create({
-  baseURL,
+  baseURL
 });
 
 export default api;
